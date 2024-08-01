ab41fcdfd9c2ac114fc777a40120718ac72407d3
RavenMonlith <86017758+Aymane-Khaldi03@users.noreply.github.com>
2024-08-01 02:30:31 +0100

    Update TelephoneLineLanding.js

--- server/client/src/components/Dashboard/TelephoneLineLanding.js
+++ server/client/src/components/Dashboard/TelephoneLineLanding.js
@@ -1,57 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
-import Modal from 'react-modal';
-import { useAuth } from '../../hooks/useAuth'; // Ensure you have this hook to get user info
 import './TelephoneLineLanding.css';
-
-Modal.setAppElement('#root');
 
 const TelephoneLineLanding = () => {
   const history = useHistory();
-  const { user } = useAuth(); // Get the user info including the role
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-  };
-
-  const handleModifyClick = () => {
-    if (user && user.role === 'admin') {
-      history.push('/telephone-line-manager'); // Change this path to the modify page path
-    } else {
-      openModal();
-    }
-  };
 
   return (
     <div className="telephone-landing-container">
       <div className="telephone-card telephone-view-card" onClick={() => history.push('/telephone-line-view')}>
         <div className="telephone-card-content">
-          <h3>Afficher Line Téléphonique</h3>
+          <h3>Afficher GSM</h3>
           <p>⦿Voir tableau <br />⦿Exporter Excel</p>
         </div>
       </div>
-      <div className="telephone-card telephone-modify-card" onClick={handleModifyClick}>
+      <div className="telephone-card telephone-modify-card" onClick={() => history.push('/telephone-line-manager')}>
         <div className="telephone-card-content">
-          <h3>Modifier Line Téléphonique</h3>
+          <h3>Modifier GSM</h3>
           <p>⦿Plus d'infos</p>
         </div>
       </div>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Access Denied"
-        className="modal"
-        overlayClassName="overlay"
-      >
-        <h2>Accès Refusé</h2>
-        <p>Vous n'avez pas la permission d'accéder à cette page.</p>
-        <button onClick={closeModal}>Fermer</button>
-      </Modal>
     </div>
   );
 };
