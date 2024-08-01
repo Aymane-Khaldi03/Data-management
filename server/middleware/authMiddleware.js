e7b44ec9a10833771b4373e6047b644789f47e62
RavenMonlith <86017758+Aymane-Khaldi03@users.noreply.github.com>
2024-08-01 02:33:12 +0100

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,30 +2,38 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const authenticate = async (req, res, next) => {
-  console.log("Authenticating request...");
+  console.log('Authenticating request...');
   const token = req.header('Authorization')?.replace('Bearer ', '');
+  console.log('Token:', token);
 
   if (!token) {
+    console.log('No token, authorization denied');
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findByPk(decoded.user.id);
+    console.log('Decoded token:', decoded);
 
-    if (!req.user) {
-      return res.status(401).json({ msg: 'Token is not valid' });
+    req.user = await User.findByPk(decoded.user.id);
+    console.log('Authenticated user:', req.user);
+
+    if (!req.user || (req.user.role !== 'admin' && !req.user.isValidated)) {
+      console.log('Token is not valid or user not validated');
+      return res.status(401).json({ msg: 'Token is not valid or user not validated' });
     }
 
     next();
   } catch (err) {
-    console.error(err.message);
+    console.error('Token verification error:', err.message);
     res.status(401).json({ msg: 'Token is not valid' });
   }
 };
 
 const isAdmin = (req, res, next) => {
+  console.log('Checking if user is admin:', req.user);
   if (req.user.role !== 'admin') {
+    console.log('Access denied. Admins only.');
     return res.status(403).json({ msg: 'Access denied. Admins only.' });
   }
   next();
